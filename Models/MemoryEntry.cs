e38848621f834bb9642dcd24041cb23141461954
Daniel A <daniel.aldemir79@gmail.com>
2026-09-18 22:04:49 +0200

    Validerar obligatoriska fält i minnen

--- Models/MemoryEntry.cs
+++ Models/MemoryEntry.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Glimt.Api.Models;
 
 public class MemoryEntry
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Titel är obligatorisk.")]
     public string Title { get; set; } = string.Empty;
 
     public DateOnly Date { get; set; }
 
+    [Required(ErrorMessage = "Beskrivning är obligatorisk.")]
     public string Description { get; set; } = string.Empty;
 
     // Databasen sparar bildens sökväg, inte själva bildfilen
